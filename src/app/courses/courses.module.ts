dea9512767efe41f44285a9a795ce9eb8c8a2e0b
Pavel Khlebko <proto@tut.by>
2018-06-22 14:30:05 +0300

    Added pipe for videoDuration

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesListComponent } from './courses-list/courses-list.component';
+import { VideoDirationPipe } from './video-duration.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CoursesListComponent, CoursesListComponent]
+  declarations: [
+    CoursesListComponent,
+    CoursesListComponent,
+    VideoDirationPipe
+  ]
 })
 export class CoursesModule { }
---
+++ src/app/courses/video-duration.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'videoDiration'})
+export class VideoDirationPipe implements PipeTransform {
+  transform(value: string, format: string): string {
+    const timeSpan = parseInt(value, 10);
+    const hour = Math.floor(timeSpan / 60);
+    const min = timeSpan % 60;
+
+    return format.replace('h', joinWithUnit(hour, ['h'])).replace('m', joinWithUnit(min, ['minute', 'munutes']));
+  }
+}
+
+function joinWithUnit(time: number, unit: string[]) {
+  if (time > 0) {
+    if (time > 1 && unit.length > 1) {
+      return time.toString() + unit[1];
+    }
+
+    return time.toString() + unit[0];
+  }
+  return '';
+}
