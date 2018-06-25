73ff5a7ccc55e85630717b364a1a57b0b85ece0e
Pavel Khlebko <proto@tut.by>
2018-06-25 13:08:10 +0300

    Added all lifcycle hooks

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,16 +1,36 @@
-import { Component, Input, OnInit, OnChanges,  SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges, AfterContentInit, AfterContentChecked, AfterViewInit,
+  AfterViewChecked, OnDestroy, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit, OnChanges {
+export class HeaderComponent implements OnInit, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit,
+  AfterViewChecked, OnDestroy, DoCheck {
   @Input() title: string;
   constructor() { }
   ngOnInit() {
   }
   ngOnChanges(changes: SimpleChanges) {
     console.log(changes);
+  }
+  ngAfterContentInit() {
+    console.log('AfterContentInit');
+  }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+  ngDoCheck() {
+    console.log('ngDoCheck');
   }
 }
