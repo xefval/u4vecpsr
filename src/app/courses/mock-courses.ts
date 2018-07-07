c0231b91372bebef1659a431454b7014a6a12745
xefval <40235249+xefval@users.noreply.github.com>
2018-07-07 10:29:44 +0300

    Fixed date in moc

--- src/app/courses/mock-courses.ts
+++ src/app/courses/mock-courses.ts
@@ -1,10 +1,10 @@
 import { CourseItem } from './course-item';
 
 export const COURSE_ITEMS: CourseItem[] = [
-  new CourseItem(1, 'Course #1', new Date(), 70, 'Course description #1'),
-  new CourseItem(2, 'Course #2', new Date(), 35, 'Course description #2'),
-  new CourseItem(3, 'Course #3', new Date(), 60, 'Course description #3'),
-  new CourseItem(3, 'Course #4', new Date(), 121, 'Course description #4'),
-  new CourseItem(3, 'Course #5', new Date(), 164, 'Course description #5'),
-  new CourseItem(3, 'Course #6', new Date(), 99, 'Course description #6')
+  new CourseItem(1, 'Course #1', 1530948051000, 70, 'Course description #1'),
+  new CourseItem(2, 'Course #2', 1530948052000, 35, 'Course description #2'),
+  new CourseItem(3, 'Course #3', 1530948053000, 60, 'Course description #3'),
+  new CourseItem(3, 'Course #4', 1530948054000, 121, 'Course description #4'),
+  new CourseItem(3, 'Course #5', 1530948055000, 164, 'Course description #5'),
+  new CourseItem(3, 'Course #6', 1530948056000, 99, 'Course description #6')
 ];
