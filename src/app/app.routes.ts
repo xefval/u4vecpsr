276d3a96160cd7701395858cfe097740a396ba5b
Pavel Khlebko <proto@tut.by>
2018-08-02 11:27:26 +0300

    Added canActivate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,25 @@ import { LoginComponent } from './users/login/login.component';
 import { CoursesListComponent } from './courses/courses-list/courses-list.component';
 import { CreateCourseComponent } from './courses/create-course/create-course.component';
 import { Page404Component } from './core/page404/page404.component';
+import { CanActivateGuard } from './users/can-activate';
 
 export const ROUTES: Route[] = [
-  { path: '', redirectTo: '/courses', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   {
     path: 'courses',
-    component: CoursesListComponent
+    component: CoursesListComponent,
+    canActivate: [CanActivateGuard]
   },
   {
     path: 'courses/:id',
-    component: CreateCourseComponent
+    component: CreateCourseComponent,
+    canActivate: [CanActivateGuard]
   },
-  { path: 'courses/new', component: CreateCourseComponent },
+  {
+    path: 'courses/new',
+    component: CreateCourseComponent,
+    canActivate: [CanActivateGuard]
+  },
   { path: 'login', component: LoginComponent },
   { path: '**', component: Page404Component }
 ];
--- src/app/users/can-activate.ts
+++ src/app/users/can-activate.ts
@@ -1,3 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
+
+@Injectable()
+export class CanActivateGuard implements CanActivate {
+  constructor() {
+
+  }
+
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }
+}
